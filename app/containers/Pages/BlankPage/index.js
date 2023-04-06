8351ac7443157d3cb3b7b8d77d3f7c58f1da3661
Nimra Khalid <75177469+Nimra-Khalid@users.noreply.github.com>
2023-04-06 16:53:20 +0500

    component

--- app/containers/Pages/BlankPage/index.js
+++ app/containers/Pages/BlankPage/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import brand from 'boss-api/dummy/brand';
 import { PapperBlock } from 'boss-components';
+import LimitedBadges from '../../../components/Badges/LimitedBadges';
 
 function BlankPage() {
   const title = brand.name + ' - Blank Page';
@@ -10,14 +11,19 @@ function BlankPage() {
     <div>
       <Helmet>
         <title>{title}</title>
-        <meta name="description" content={description} />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="twitter:title" content={title} />
-        <meta property="twitter:description" content={description} />
+        <meta name='description' content={description} />
+        <meta property='og:title' content={title} />
+        <meta property='og:description' content={description} />
+        <meta property='twitter:title' content={title} />
+        <meta property='twitter:description' content={description} />
       </Helmet>
-      <PapperBlock title="Blank Page" desc="Some text description">
+      <PapperBlock title='Blank Page' desc='Some text description'>
         Content
+        <div>
+          <LimitedBadges limit={10} value={2}>
+            Badge
+          </LimitedBadges>
+        </div>
       </PapperBlock>
     </div>
   );
